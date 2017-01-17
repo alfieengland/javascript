6654c1c1a0286a308582db74003d3fd6af2cf871
unknown <16434320@NW041-15056.eng.nene.ac.uk>
2017-01-17 15:18:58 +0000

    Added console print function

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ function print5() {
   alert(5);
 }
 
+/*
 print5();
 print5();
+*/
+
+function console5() {
+  console.log('1');
+  console.log('2');
+  console.log('3');
+  console.log('4');
+  console.log('5');
+}
+
+console5();
+console5();
